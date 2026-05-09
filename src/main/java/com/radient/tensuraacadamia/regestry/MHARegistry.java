9bf3ca3b0a87ad457a8f6d7f912d9aa8e5d7bdb4
Radient Fox <ink2.0games@gmail.com>
2026-05-08 23:30:31 -0700

    initial commit

---
+++ src/main/java/com/radient/tensuraacadamia/regestry/MHARegistry.java
@@ -0,0 +1,14 @@
+package com.radient.tensuraacadamia.regestry;
+
+
+
+public class MHARegistry {
+
+
+    public static void register(net.neoforged.bus.api.IEventBus modEventBus) {
+
+
+
+    }
+
+}
