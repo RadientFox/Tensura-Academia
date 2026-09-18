7971abb8444fa4879d2b3c2ad645a5d34a56241d
Hallow <92547397+HallowOVA@users.noreply.github.com>
2026-09-17 20:41:03 -0500

    Dangersense

--- src/main/java/com/radient/tensuraacadamia/regestry/skills/QuirkSkills.java
+++ src/main/java/com/radient/tensuraacadamia/regestry/skills/QuirkSkills.java
@@ -1,10 +1,7 @@
 package com.radient.tensuraacadamia.regestry.skills;
 
 import com.radient.tensuraacadamia.ability.ultimate.ofausers.OFA1st;
-import com.radient.tensuraacadamia.ability.unique.quirks.FloatQuirk;
-import com.radient.tensuraacadamia.ability.unique.quirks.GearshiftQuirk;
-import com.radient.tensuraacadamia.ability.unique.quirks.Power_Stock;
-import com.radient.tensuraacadamia.ability.unique.quirks.QuirkBestowal;
+import com.radient.tensuraacadamia.ability.unique.quirks.*;
 import dev.architectury.registry.registries.RegistrySupplier;
 import io.github.manasmods.manascore.skill.api.ManasSkill;
 import io.github.manasmods.manascore.skill.impl.SkillRegistry;
@@ -22,6 +19,7 @@ public class QuirkSkills {
     public static final RegistrySupplier<QuirkBestowal> QUIRK_BESTOWAL = register("quirk_bestowal", QuirkBestowal::new);
     public static final RegistrySupplier<FloatQuirk> FLOAT = register("float", FloatQuirk::new);
     public static final RegistrySupplier<GearshiftQuirk> GEARSHIFT = register("gearshift", GearshiftQuirk::new);
+    public static final RegistrySupplier<DangerSenseQuirk> DANGERSENSE = register("dangersense", DangerSenseQuirk::new);
 
     private static <E extends ManasSkill> RegistrySupplier<E> register(String name, Supplier<E> supplier) {
         return SkillRegistry.SKILLS.register(ResourceLocation.fromNamespaceAndPath("tracadamia", name), supplier);
